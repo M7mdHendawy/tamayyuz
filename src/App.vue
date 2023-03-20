8749501143d2a1da8eb0f62df5e8cf7a89fbe705
MohammedFathi <mohamed@easy-trademarks.com>
2023-03-20 07:52:01 +0200

    Updating css

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div v-for="(ex, key) in excercises" :key="ex">
+    <h1>{{ key }}</h1>
+    <table class="container--table">
+      <tr>
+        <td class="lighten-red">الفصل</td>
+        <td class="lighten-red">المادة</td>
+        <td class="lighten-red">الحفظ</td>
+        <td class="lighten-red">الدراسة</td>
+      </tr>
+
+      <tr v-for="(items, values) in ex" :key="values">
+        <td class="lighten-red">{{ values }}</td>
+        <td v-for="name in items" :key="name">
+          <table class="inner--table" v-for="value in name" :key="value">
+            <td>
+              {{ value }}
+            </td>
+          </table>
+        </td>
+      </tr>
+    </table>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+  data() {
+    return {
+      excercises: [],
+    };
+  },
+  mounted() {
+    fetch("http://localhost:3000/db")
+      .then((response) => response.json())
+      .then((response) => (this.excercises = response))
+      .then((data) => {
+        // Convert JSON text to JavaScript object
+        console.table(data);
+
+      })
+      .catch((error) => console.error(error));
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import './assets/style.css'
 </style>
